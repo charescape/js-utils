46dc07025cea860d6a8bef54a41814ab438b3552
charescape <charescape@outlook.com>
2021-04-18 02:59:00 +0800

    is

---
+++ src/is.js
@@ -0,0 +1,40 @@
+export function isUndefined(value) {
+  return value === undefined;
+}
+
+export function isNull(value) {
+  return value === null;
+}
+
+export function isNil(value) {
+  return isUndefined(value) || isNull(value);
+}
+
+export function isTrue(value) {
+  return value === true
+}
+
+export function isFalse(value) {
+  return value === false
+}
+
+export function isObject(value) {
+  return (typeof value === 'object') && isNil(value) === false && Array.isArray(value) === false;
+}
+
+export function isPlainObject(value) {
+  if (!isObject(value)) {
+    return false;
+  }
+  if (isUndefined(value.constructor)) {
+    return true;
+  }
+  if (!isObject(value.constructor.prototype)) {
+    return false;
+  }
+  if (value.constructor.prototype.hasOwnProperty('isPrototypeOf') === false) {
+    return false;
+  }
+
+  return true;
+}
