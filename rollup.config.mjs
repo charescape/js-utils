17222f03938ceb7e631691556b3f80ad77d8ada0
charescape <charescape@outlook.com>
2021-04-19 14:33:25 +0800

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,45 +7,54 @@ import { terser } from 'rollup-plugin-terser';
 const pkgs = [];
 
 [
+  {name: 'index'},
   {name: 'is'},
 ].forEach((pkg) => {
+  const isCombined = pkg.name === 'index';
+
   const inputPath = `src/${pkg.name}.js`;
-  const outputName = `JsUtils_${_upperFirst(_camelCase(pkg.name))}`;
+  let outputName = `JsUtils`;
+  let outputFileName = `js-utils`;
+
+  if (!isCombined) {
+    outputName += `_${_upperFirst(_camelCase(pkg.name))}`;
+    outputFileName += `.${pkg.name}`;
+  }
 
   pkgs.push({
     input: inputPath,
     output: [
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.cjs.js`,
+        file: `dist/${outputFileName}.cjs.js`,
         format: 'cjs'
       },
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.esm.js`,
+        file: `dist/${outputFileName}.esm.js`,
         format: 'esm'
       },
 
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.iife.js`,
+        file: `dist/${outputFileName}.iife.js`,
         format: 'iife'
       },
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.iife.min.js`,
+        file: `dist/${outputFileName}.iife.min.js`,
         format: 'iife',
         plugins: [terser()]
       },
 
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.umd.js`,
+        file: `dist/${outputFileName}.umd.js`,
         format: 'umd'
       },
       {
         name: outputName,
-        file: `dist/js-utils.${pkg.name}.umd.min.js`,
+        file: `dist/${outputFileName}.umd.min.js`,
         format: 'umd',
         plugins: [terser()]
       }
