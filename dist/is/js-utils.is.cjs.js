877fdaad631c7448967eafa022ab79cbf2539f5c
charescape <charescape@outlook.com>
2021-04-20 00:37:00 +0800

    dist

--- dist/is/js-utils.is.cjs.js
+++ dist/is/js-utils.is.cjs.js
@@ -3,104 +3,79 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 function isUndefined(value) {
-  return value === undefined;
+    return value === undefined;
 }
-
 function isNull(value) {
-  return value === null;
+    return value === null;
 }
-
 function isNil(value) {
-  return value === undefined || value === null;
+    return value === undefined || value === null;
 }
-
-
 function isTrue(value) {
-  return value === true;
+    return value === true;
 }
-
 function isFalse(value) {
-  return value === false;
+    return value === false;
 }
-
 function isBool(value) {
-  return value === true || value === false;
+    return value === true || value === false;
 }
-
-
 function isNumber(value) {
-  return (typeof value === "number");
+    return (typeof value === "number");
 }
-
 function isString(value) {
-  return (typeof value === "string");
+    return (typeof value === "string");
 }
-
 function isStringFilled(value) {
-  return isString(value) && value.trim() !== "";
+    return isString(value) && value.trim() !== "";
 }
-
 function isStringEmpty(value) {
-  return isString(value) && value.trim() === "";
+    return isString(value) && value.trim() === "";
 }
-
 function isArray(value) {
-  return Array.isArray(value);
+    return Array.isArray(value);
 }
-
 function isArrayFilled(value) {
-  return isArray(value) && value.length > 0;
+    return isArray(value) && value.length > 0;
 }
-
 function isArrayEmpty(value) {
-  return isArray(value) && value.length === 0;
+    return isArray(value) && value.length === 0;
 }
-
 function isObject(value) {
-  return (typeof value === "object") && !isNil(value) && !isArray(value);
+    return (typeof value === "object") && !isNil(value) && !isArray(value);
 }
-
 function isObjectFilled(value) {
-  return isObject(value) && Object.keys(value).length > 0;
+    return isObject(value) && Object.keys(value).length > 0;
 }
-
 function isObjectEmpty(value) {
-  return isObject(value) && Object.keys(value).length === 0;
+    return isObject(value) && Object.keys(value).length === 0;
 }
-
 function isFunction(value) {
-  return (typeof value === "function") && isObject(value);
+    return (typeof value === "function") && isObject(value);
 }
-
 function isPlainObject(value) {
-  if (!isObject(value)) {
-    return false;
-  }
-
-  // eg: Math [object Math], Error [object Function] ...
-  if (Object.prototype.toString.call(value) !== "[object Object]") {
-    return false;
-  }
-
-  // following lodash: https://github.com/lodash/lodash/blob/master/isPlainObject.js#L34-L41
-  if (Object.getPrototypeOf(value) === null) {
-    return true;
-  }
-
-  let prototypeOf = value;
-  while (Object.getPrototypeOf(prototypeOf) !== null) {
-    prototypeOf = Object.getPrototypeOf(prototypeOf);
-  }
-
-  return Object.getPrototypeOf(value) === prototypeOf;
+    if (!isObject(value)) {
+        return false;
+    }
+    // eg: Math [object Math], Error [object Function] ...
+    if (Object.prototype.toString.call(value) !== "[object Object]") {
+        return false;
+    }
+    // following lodash: https://github.com/lodash/lodash/blob/master/isPlainObject.js#L34-L41
+    if (Object.getPrototypeOf(value) === null) {
+        return true;
+    }
+    var prototypeOf = value;
+    while (Object.getPrototypeOf(prototypeOf) !== null) {
+        prototypeOf = Object.getPrototypeOf(prototypeOf);
+    }
+    return Object.getPrototypeOf(value) === prototypeOf;
 }
-
 function isStringContainsString(haystack, needle) {
-  if (!isString(haystack) || !isString(needle)) {
-    return false;
-  }
-
-  return haystack.indexOf(needle) !== -1;
+    if (!isString(haystack) || !isString(needle)) {
+        return false;
+    }
+    return haystack.indexOf(needle) !== -1;
 }
 
 exports.isArray = isArray;
--- dist/js-utils.umd.js
+++ dist/js-utils.umd.js
@@ -1,130 +1,105 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.JsUtils = {}));
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.JsUtils = {}));
 }(this, (function (exports) { 'use strict';
 
-  function isUndefined(value) {
-    return value === undefined;
-  }
-
-  function isNull(value) {
-    return value === null;
-  }
-
-  function isNil(value) {
-    return value === undefined || value === null;
-  }
-
-
-  function isTrue(value) {
-    return value === true;
-  }
-
-  function isFalse(value) {
-    return value === false;
-  }
-
-  function isBool(value) {
-    return value === true || value === false;
-  }
-
-
-  function isNumber(value) {
-    return (typeof value === "number");
-  }
-
-  function isString(value) {
-    return (typeof value === "string");
-  }
-
-  function isStringFilled(value) {
-    return isString(value) && value.trim() !== "";
-  }
-
-  function isStringEmpty(value) {
-    return isString(value) && value.trim() === "";
-  }
-
-  function isArray(value) {
-    return Array.isArray(value);
-  }
-
-  function isArrayFilled(value) {
-    return isArray(value) && value.length > 0;
-  }
-
-  function isArrayEmpty(value) {
-    return isArray(value) && value.length === 0;
-  }
-
-  function isObject(value) {
-    return (typeof value === "object") && !isNil(value) && !isArray(value);
-  }
-
-  function isObjectFilled(value) {
-    return isObject(value) && Object.keys(value).length > 0;
-  }
-
-  function isObjectEmpty(value) {
-    return isObject(value) && Object.keys(value).length === 0;
-  }
-
-  function isFunction(value) {
-    return (typeof value === "function") && isObject(value);
-  }
-
-  function isPlainObject(value) {
-    if (!isObject(value)) {
-      return false;
+    function isUndefined(value) {
+        return value === undefined;
+    }
+    function isNull(value) {
+        return value === null;
+    }
+    function isNil(value) {
+        return value === undefined || value === null;
+    }
+    function isTrue(value) {
+        return value === true;
+    }
+    function isFalse(value) {
+        return value === false;
+    }
+    function isBool(value) {
+        return value === true || value === false;
+    }
+    function isNumber(value) {
+        return (typeof value === "number");
+    }
+    function isString(value) {
+        return (typeof value === "string");
+    }
+    function isStringFilled(value) {
+        return isString(value) && value.trim() !== "";
+    }
+    function isStringEmpty(value) {
+        return isString(value) && value.trim() === "";
+    }
+    function isArray(value) {
+        return Array.isArray(value);
+    }
+    function isArrayFilled(value) {
+        return isArray(value) && value.length > 0;
+    }
+    function isArrayEmpty(value) {
+        return isArray(value) && value.length === 0;
+    }
+    function isObject(value) {
+        return (typeof value === "object") && !isNil(value) && !isArray(value);
+    }
+    function isObjectFilled(value) {
+        return isObject(value) && Object.keys(value).length > 0;
+    }
+    function isObjectEmpty(value) {
+        return isObject(value) && Object.keys(value).length === 0;
+    }
+    function isFunction(value) {
+        return (typeof value === "function") && isObject(value);
+    }
+    function isPlainObject(value) {
+        if (!isObject(value)) {
+            return false;
+        }
+        // eg: Math [object Math], Error [object Function] ...
+        if (Object.prototype.toString.call(value) !== "[object Object]") {
+            return false;
+        }
+        // following lodash: https://github.com/lodash/lodash/blob/master/isPlainObject.js#L34-L41
+        if (Object.getPrototypeOf(value) === null) {
+            return true;
+        }
+        var prototypeOf = value;
+        while (Object.getPrototypeOf(prototypeOf) !== null) {
+            prototypeOf = Object.getPrototypeOf(prototypeOf);
+        }
+        return Object.getPrototypeOf(value) === prototypeOf;
+    }
+    function isStringContainsString(haystack, needle) {
+        if (!isString(haystack) || !isString(needle)) {
+            return false;
+        }
+        return haystack.indexOf(needle) !== -1;
     }
 
-    // eg: Math [object Math], Error [object Function] ...
-    if (Object.prototype.toString.call(value) !== "[object Object]") {
-      return false;
-    }
+    exports.isArray = isArray;
+    exports.isArrayEmpty = isArrayEmpty;
+    exports.isArrayFilled = isArrayFilled;
+    exports.isBool = isBool;
+    exports.isFalse = isFalse;
+    exports.isFunction = isFunction;
+    exports.isNil = isNil;
+    exports.isNull = isNull;
+    exports.isNumber = isNumber;
+    exports.isObject = isObject;
+    exports.isObjectEmpty = isObjectEmpty;
+    exports.isObjectFilled = isObjectFilled;
+    exports.isPlainObject = isPlainObject;
+    exports.isString = isString;
+    exports.isStringContainsString = isStringContainsString;
+    exports.isStringEmpty = isStringEmpty;
+    exports.isStringFilled = isStringFilled;
+    exports.isTrue = isTrue;
+    exports.isUndefined = isUndefined;
 
-    // following lodash: https://github.com/lodash/lodash/blob/master/isPlainObject.js#L34-L41
-    if (Object.getPrototypeOf(value) === null) {
-      return true;
-    }
-
-    let prototypeOf = value;
-    while (Object.getPrototypeOf(prototypeOf) !== null) {
-      prototypeOf = Object.getPrototypeOf(prototypeOf);
-    }
-
-    return Object.getPrototypeOf(value) === prototypeOf;
-  }
-
-  function isStringContainsString(haystack, needle) {
-    if (!isString(haystack) || !isString(needle)) {
-      return false;
-    }
-
-    return haystack.indexOf(needle) !== -1;
-  }
-
-  exports.isArray = isArray;
-  exports.isArrayEmpty = isArrayEmpty;
-  exports.isArrayFilled = isArrayFilled;
-  exports.isBool = isBool;
-  exports.isFalse = isFalse;
-  exports.isFunction = isFunction;
-  exports.isNil = isNil;
-  exports.isNull = isNull;
-  exports.isNumber = isNumber;
-  exports.isObject = isObject;
-  exports.isObjectEmpty = isObjectEmpty;
-  exports.isObjectFilled = isObjectFilled;
-  exports.isPlainObject = isPlainObject;
-  exports.isString = isString;
-  exports.isStringContainsString = isStringContainsString;
-  exports.isStringEmpty = isStringEmpty;
-  exports.isStringFilled = isStringFilled;
-  exports.isTrue = isTrue;
-  exports.isUndefined = isUndefined;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
+    Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
