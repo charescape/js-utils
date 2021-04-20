5cdbbbe14d89d97594023a02ea8e581de6f79dc4
charescape <charescape@outlook.com>
2021-04-20 12:32:56 +0800

    dist

--- dist/ajax/js-utils.ajax.iife.js
+++ dist/ajax/js-utils.ajax.iife.js
@@ -1,13 +1,34 @@
 var JsUtils_Ajax = (function (exports) {
     'use strict';
 
-    function create(axios) {
-        return axios.create({
-            timeout: 60 * 1000,
-        });
+    function ajaxCreate(axios, config) {
+        // see https://github.com/axios/axios/blob/master/lib/defaults.js
+        const defaultConfig = {
+            method: 'GET',
+            timeout: 0,
+            // paramsSerializer: function (params) {
+            //   return Qs.stringify(params, {arrayFormat: 'brackets'})
+            // },
+            // see https://github.com/axios/axios/blob/master/lib/adapters/xhr.js#L126
+            // see https://github.com/axios/axios/blob/master/test/specs/xsrf.spec.js#L70-L81
+            // xsrfCookieName: 'XSRF-TOKEN', // default
+            // xsrfHeaderName: 'X-XSRF-TOKEN', // default
+            withCredentials: false,
+            // can be: [arraybuffer] [document] [json] [text] [stream] [blob](browser only)
+            responseType: 'json',
+        };
+        const internalConfig = Object.assign(Object.assign({}, defaultConfig), config);
+        // if (!isPlainObject(config.headers)) {
+        //   config.headers = {};
+        // }
+        // @ts-ignore
+        return axios.create(internalConfig);
+        // instance.defaults.baseURL = 'https://api.example.com';
+        // instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+        // instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     }
 
-    exports.create = create;
+    exports.ajaxCreate = ajaxCreate;
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
